9634bd3aa168095420cf9df37fd74b05a89f1385
Amanda <amandaputri@student.telkomuniversity.ac.id>
2022-05-23 23:43:02 +0700

    .Membuat navigasi ke app lain

--- app/src/main/java/org/d3if2036/hitungkecepatan/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if2036/hitungkecepatan/ui/HitungFragment.kt
@@ -1,5 +1,6 @@
 package org.d3if2036.hitungkecepatan.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
@@ -36,6 +37,7 @@ class HitungFragment : Fragment() {
                 R.id.action_hitungFragment_to_rumusFragment
             )
         }
+        binding.shareButton.setOnClickListener { shareData() }
         viewModel.getHasilKecepatan().observe(requireActivity(), {showResult(it)})
     }
 
@@ -75,6 +77,20 @@ class HitungFragment : Fragment() {
     private fun showResult(result: HasilKecepatan?){
         if (result == null) return
         binding.total.text = getString(R.string.hasilKecepatan, result.kecepatan)
-        binding.RumusButton.visibility = View.VISIBLE
+        binding.buttonGroup.visibility = View.VISIBLE
+    }
+
+    private fun shareData(){
+        val message = getString(R.string.bagikantemp,
+            binding.jarakInp.text,
+            binding.waktuInp.text,
+            binding.total.text)
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager) != null) {
+            startActivity(shareIntent)
+        }
     }
 }
